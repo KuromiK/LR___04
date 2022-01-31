084f95afc2d9e7060d6919cc7c31cb300ca65026
KuromiK <98142015+KuromiK@users.noreply.github.com>
2022-01-31 20:41:59 +0300

    Update dubl.py

--- dubl.py
+++ dubl.py
@@ -5,8 +5,13 @@ def g():
   pass
 
 
-def dictionary():
-  pass
+def dictionary(n, t={}):
+   for i in os.listdir(n):
+        if os.path.isdir(n + '\\' + i):
+            dictionari(n + '\\' + i)
+        elif os.path.isfile(n + '\\' + i):
+            t[n + '\\' + i] = os.stat(n + '\\' + i).st_size
+    return t
 
 
 def duplicate():
